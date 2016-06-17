dc5fb9462fff7d55c7646289a4f197cec4328d79
Leo Gordon <lg4@ebi.ac.uk>
2016-06-17 13:09:53 +0100

    Allow editing of the RNG file; does not yet support internal reloading

--- parser.js
+++ parser.js
@@ -12,9 +12,6 @@
  * limitations under the License.
  */ 
 
-
-var xmlParser=new DOMParser();
-var xmlDoc;
 var elements=[];
 var blocks=[];
 var blockNames=[];
@@ -24,38 +21,37 @@ var optionalNames=[];
 //init function for initializing the Blockly block area
 function init(){
 	var x=Blockly.inject('blocklyDiv', {
-		toolbox: document.getElementById('startBlocks'),
+        toolbox: document.getElementById('toolbox'),
         collapse: true
 	});
 }
-		
-//reads the file
+
+// loads the file into RNG textarea and leaves it there for potential manual edit
 function readFile(evt){
-	var file=evt.target.files[0];
-	var reader=new FileReader();
-	reader.readAsText(file);
-	reader.onload=function(e){
-		console.log(e.target.result);
-		xmlDoc=xmlParser.parseFromString(e.target.result,"text/xml");
-		handleXML();
-	}
+    var file=evt.target.files[0];
+    var reader=new FileReader();
+    reader.readAsText(file);
+    reader.onload=function(e){
+        document.getElementById('rng_area').value = e.target.result;
+    }
 }
 		
 //handles xml by creating blocks as per RNG rules
-function handleXML(){
-	var root=xmlDoc.documentElement;
-	var a;
+function handleRNG( unparsedRNG ){
+
+    var xmlParser=new DOMParser();
+    var rngDoc=xmlParser.parseFromString(unparsedRNG, "text/xml");
 	
-	removeRedundantText(root);
+	removeRedundantText(rngDoc.documentElement);
 		
 	//get all the define blocks in the document and create blocks for them one by one.
-	var defineNodes=xmlDoc.getElementsByTagName("define");
+	var defineNodes=rngDoc.getElementsByTagName("define");
 	for(var i=0;i<defineNodes.length;i++){
 		//fixed color - 295 for define blocks for now.
 		createBlocks(defineNodes[i],defineNodes[i].getAttribute("name"), 295);
 	}
 		
-	root=xmlDoc.getElementsByTagName("start")[0];
+	var root=rngDoc.getElementsByTagName("start")[0];
 
 	var colour=0;
 	createBlocks(root,"start",colour);
@@ -67,7 +63,7 @@ function handleXML(){
 		resultsData.innerHTML=resultsData.innerHTML+"<p>"+blocks[i]+"</p>"
 		script.text+=blocks[i];
 			
-		var blocklyXml=document.getElementById('startBlocks');
+		var blocklyXml=document.getElementById('toolbox');
 		console.log(blockNames[i]);
 		blocklyXml.innerHTML=blocklyXml.innerHTML+"<block type='"+blockNames[i]+"'></block>";
 	}
@@ -277,9 +273,6 @@ function createBlocks(node, name, colour){
 	return blockData;
 }
 		
-//Adds an event listener for detecting whether the user has provided a file as input or not.
-document.getElementById("file").addEventListener('change',readFile);
-
 
 //this commented part contains a few things that were tried out(not necessarily together) to validate oneOrMore but it didn't work properly. Kept now in case some idea from here is required in future.
 /*
@@ -387,3 +380,4 @@ function checker(){
 		//console.log(source.inputList);
 	}	
 }
+
