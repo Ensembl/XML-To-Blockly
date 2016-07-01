1df71acae6fb42110a89af3d838bb1965bb381de
anujk14 <khandelwal.anuj14@gmail.com>
2016-07-01 16:38:06 +0530

    Fixed _removeNodeNameRecursively() to handle comments and texts in different ways

--- parser.js
+++ parser.js
@@ -93,13 +93,13 @@ function removeXMLComments(node) {
 function _removeNodeNameRecursively(node, name) {
 	var children=node.childNodes;
 	for(var i=0;i<children.length;i++){
-		if(children[i].nodeName == name && children[i].nodeValue.trim()==""){
+		if( (name == "#comment" && children[i].nodeName == name) || (children[i].nodeName == name && children[i].nodeValue.trim()=="") ){
 			children[i].parentNode.removeChild(children[i]);
 			i--;
 			continue;
 		}else{
 			_removeNodeNameRecursively(children[i], name);
-		}
+		}	
 	}
 }
 		
