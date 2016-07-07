bd3d0cc79dc50e4c56a3555da4578f8ee6bb160e
anujk14 <khandelwal.anuj14@gmail.com>
2016-07-07 20:07:19 +0530

    Cleaned up the code by creating functions for better readability

--- testTool.js
+++ testTool.js
@@ -1,3 +1,17 @@
+/* 	This file can be used as follows:
+ *
+ *	phantomjs testTool.js   <input folder path (optional)>   <result folder path(optional)>
+ *
+ *	OR
+ *
+ *	phantomjs testTool.js   <file name (optional)>   <results folder path(optional)>
+ *
+ *	If input folder path isn't specified, "rng files" folder in the repo is used as default folder for input files
+ * 	If output folder path isn't specified, "expected results" folder in the repo is used as default folder to look for expected results
+ *
+ */
+ 
+ 
 var fs = require('fs');
 var system = require('system');
 var webPage = require('webpage');
@@ -9,21 +23,21 @@ var inputFolderName = "rng files";
 var resultsDirectory = "expected results";
 var failedTestNames = [];
 
-if(system.args.length > 1){
+if(system.args.length > 1){					//system.args[0] is the filename itself
 	inputFolderName = system.args[1];
 	if(system.args.length == 3){
 		resultsDirectory = system.args[2];
 	}
 }
 
-//system.args[0] is the filename itself
 var fileList = fs.list(inputFolderName);
 
+//if the user has entered a file name, fileList's length will be 0
 if(fileList.length == 0){
 	var i=inputFolderName.indexOf(".rng");
 	for(; i>=0; i--){
 		var c=inputFolderName.charAt(i);
-		if(c == "\\" || c=="/"){
+		if(c == "\\" || c=="/"){					
 			var x = inputFolderName.substring(i+1);
 			fileList.push(x);
 			break;
@@ -39,40 +53,30 @@ page.open("http://anujk14.github.io/XML-To-Blockly/", function(status){
 		console.log("success");
 		for(var i=0; i<fileList.length; i++){
 			console.log("Input file: "+fileList[i]);
-			var fileName = fileList[i].split(".rng")[0];
-			var expectedOutput = fs.read(resultsDirectory+"/"+fileName+".txt");
-			expectedOutput = expectedOutput.split("\n");
-			var inputForParser = "";
 			
-			if(inputFolderName.indexOf(".rng") == -1){
-				inputForParser = fs.read(inputFolderName+"/"+fileList[i]);
-			}else{
-				inputForParser = fs.read(inputFolderName);
-			}
-			var results = page.evaluate(performPageOperations, inputForParser);
+			var expectedOutput = getExpectedOutput( fileList[i] );	//get expected output for current file
 			
-			results = results.replace(new RegExp('<p>', 'g'), '');
-			results = results.split('</p>');
-			results.splice(results.length-1, 1);
+			var inputForParser = getInputForParser( fileList[i] );	//get contents of current rng file
 			
+			var results = page.evaluate(performPageOperations, inputForParser);	//send input, click interpret, get results
 			
-			var printedPrettyStatement=false;
+			var printedPrettyStatement=false;		//pretty statement here refers to statements like "Didn't find expected blocks or Found unexpected blocks"
+			
 			for(var j=0; j<expectedOutput.length; j++){
 				expectedOutput[j]=expectedOutput[j].trim();
 				if(expectedOutput[j]==""){
 					continue;
 				}
-				
 				var index = results.indexOf(expectedOutput[j]);
 				if(index == -1){
 					if(printedPrettyStatement == false){
-						console.log("Didn't find the following blocks:\n");
+						console.log("\nDidn't find the following blocks:\n");
 						printedPrettyStatement = true;
 					}
 					console.log(expectedOutput[j]);
 					console.log("\n");
 				}else{
-					results.splice(index, 1);
+					results.splice(index, 1);	//remove matching entries from results
 				}
 			}
 			
@@ -94,9 +98,6 @@ page.open("http://anujk14.github.io/XML-To-Blockly/", function(status){
 			}else{
 				passed++;
 			}
-			//console.log(expectedOutput);
-			//console.log("\n");
-			//console.log(results);
 		}
 		console.log("\nPassed: "+passed+"/"+fileList.length);
 		console.log("Failed: "+failed+"/"+fileList.length);
@@ -118,6 +119,27 @@ function performPageOperations(input){
 	document.getElementById('rng_area').value = input;
 	var btn = document.getElementById('interpretBtn');
 	btn.click();
-	//sendEvent('click', btn.offsetLeft, btn.offsetTop, 'left');
-	return document.getElementById('results').innerHTML;
+	var results = document.getElementById('results').innerHTML;
+	results = results.replace(new RegExp('<p>', 'g'), '');
+	results = results.split('</p>');
+	results.splice(results.length-1, 1);	//after splitting, the last tag is an empty one, so remove it
+	return results;
+}
+
+
+function getExpectedOutput(fileNameWithExtension){
+	var fileName = fileNameWithExtension.split(".rng")[0];
+	var expectedOutput = fs.read(resultsDirectory + "/" + fileName + ".txt");
+	expectedOutput = expectedOutput.split("\n");
+	return expectedOutput;
+}
+
+function getInputForParser(fileName){
+	var input = "";
+	if(inputFolderName.indexOf(".rng") == -1){
+		input = fs.read(inputFolderName+"/"+fileName);
+	}else{
+		input = fs.read(inputFolderName);
+	}
+	return input;
 }
