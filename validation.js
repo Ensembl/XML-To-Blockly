a1540d000b19321d5530641776951f52561608ea
Matthieu Muffato <muffato@ebi.ac.uk>
2016-08-17 21:37:02 +0100

    Added the license to all the files

--- validation.js
+++ validation.js
@@ -10,8 +10,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ */
 
- * ***************************************************************************
+/* ***************************************************************************
 
     This file contains methods that ensure that the blocks in the Blockly workspace are attached to each other in a valid sequence.
  */
