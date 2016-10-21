babfa044ec02535aaa11a329a6b2e9b400c7462d
Leo Gordon <lg4@ebi.ac.uk>
2016-10-21 16:37:45 +0100

    a minimalistic one-page interface to just do the block diagram building and XML export

--- src/js_extensions.js
+++ src/js_extensions.js
@@ -94,3 +94,21 @@ function syncLoadFileFromURL(url) {
         return;
     }
 }
+
+
+/**
+ * Simple parsing of QueryString parameters out of the URL
+ *
+ */
+function getDecodedQueryVariable(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split('&');
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+        if (decodeURIComponent(pair[0]) == variable) {
+            return decodeURIComponent(pair[1]);
+        }
+    }
+    console.log('Query variable %s not found', variable);
+    return(false);
+}
