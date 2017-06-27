4deff35ad6524716c5986a6cda1929bd52585e39
Matthieu Muffato <muffato@ebi.ac.uk>
2017-06-27 22:18:25 +0100

    Added the copyright notice

--- src/UnicodeIndenter.js
+++ src/UnicodeIndenter.js
@@ -1,4 +1,6 @@
 /*
+ * Copyright [2016-2017] Anuj Khandelwal and EMBL-European Bioinformatics Institute
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
